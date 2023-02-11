79f87cd84b13fea9d11c9bbc13846a3d407a84e9
Raman <ramanmcreates@gmail.com>
2023-02-11 01:15:43 -0500

    content styling

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,26 @@
 import Head from "next/head";
-import {
-  UserCircleIcon,
-  PhotoIcon,
-  BanknotesIcon,
-  PencilSquareIcon,
-  EnvelopeIcon,
-  PencilIcon,
-} from "@heroicons/react/24/outline";
-import {
-  LoginButton,
-  MintButton,
-  useAccount,
-  useMint,
-  useSignMessage,
-  useGetSharedSecret,
-} from "@comet-labs/react";
+import {UserCircleIcon, PhotoIcon, BanknotesIcon, PencilSquareIcon, EnvelopeIcon, PencilIcon,} 
+from "@heroicons/react/24/outline";
+import {LoginButton, MintButton, useAccount, useMint, useSignMessage, useGetSharedSecret} from "@comet-labs/react";
 import { Work_Sans } from "@next/font/google";
 const work_sans = Work_Sans({ subsets: ["latin"] });
-
 import styles from "../styles/Home.module.css";
+import Script from 'next/script'
 
 export default function Home() {
   const account = useAccount();
-
   const { startMint } = useMint({ collectionId: "22c6ecefe246" });
   const { signMessage } = useSignMessage({ message: "<MESSAGE>" });
   const { getSharedSecret } = useGetSharedSecret({
     publicKey: "Dgq5B8i5NJJfPoUgpkFZDzRr84zd1BJrUBntJt1EBvgd",
   });
 
+
+
   return (
     <>
       <Head>
-        <title>❤️❤️❤️</title>
+        <title>NFT MUSIC STORE</title>
         <meta
           name="description"
           content="The Comet React SDK lets you launch new Solana NFT collections in your app!"
@@ -42,6 +30,9 @@ export default function Home() {
       </Head>
 
       <main className={work_sans.className}>
+        <div id="embed-iframe">
+          <Script src="https://example.com/script.js" />
+        </div>
         <div className={"isolate bg-white " + styles.main}>
           <div className="px-6 pt-6 lg:px-8">
             <nav
@@ -64,55 +55,38 @@ export default function Home() {
             <div className="relative py-24 sm:py-32 lg:pb-40">
               <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl">
-                  <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                    Happy Valentines Day!!!!
+                  <h1 className="text-xl font-bold tracking-tight text-gray-900 sm:text-6xl text-center pt-px">
+                    RAMAN MAMA MUSIC NFT COLLECTION
                   </h1>
-
+                  <h1 className="text-xl font-bold tracking-tight text-gray-900 sm:text-6xl text-center pt-px text-cyan-600" >
+                    <a href="https://docs.google.com/document/d/1KUTehmCr20yh8f0YcGVIgT2kLm0MMvTJ3U_lvtv2JRI/edit?usp=sharing">
+                    CLICK ME: a note for my friends at Comet. 
+                    </a>
+                  </h1 >
                   <div className="marquee">
                     <div className="marquee__group">
-                      <img src="../kirby.jpg" width="100px" />
-                      <img src="../kirby.jpg" width="100px" />
-                      <img src="../kirby.jpg" width="100px" />
-                      <img src="../kirby.jpg" width="100px" />
-                      <img src="../kirby.jpg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
                     </div>
 
                     <div aria-hidden="true" class="marquee__group">
-                      <img src="../kirby.jpg" width="100px" />
-                      <img src="../kirby.jpg" width="100px" />
-                      <img src="../kirby.jpg" width="100px" />
-                      <img src="../kirby.jpg" width="100px" />
-                      <img src="../kirby.jpg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
+                      <img src="../tape.jpeg" width="100px" />
                     </div>
                   </div>
 
                   <div className="mt-8 flow-root max-w-2xl m-auto">
-                    <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                    <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4 bg-cyan-600">
                       <div className="bg-white rounded-md shadow-2xl ring-1 ring-gray-900/10">
                         <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-6 sm:p-6 rounded-md">
-                          <h2 className="text-xl font-bold tracking-tight text-gray-800 sm:text-2xl">
-                            Here's a token of my love and affection,
-                          </h2>
-                          {/* <div className="-m-3 flex items-start rounded-lg p-3 transition duration-150 ease-in-out hover:bg-gray-50">
-                            <div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-slate-600 text-white sm:h-12 sm:w-12">
-                              <UserCircleIcon
-                                className="h-6 w-6"
-                                aria-hidden="true"
-                              />
-                            </div>
-                            <div className="ml-4">
-                              <p className="text-base font-medium text-gray-900">
-                                Login with Comet
-                              </p>
-                              <p className="mt-1 text-sm text-gray-500 mb-3">
-                                Let your users sign in with Comet
-                              </p>
-                              <LoginButton className="z-50" />
-                            </div>
-                          </div> */}
-
-                          <h2 className="text-xl font-bold tracking-tight text-gray-800 sm:text-2xl">
-                            A Solana NFT with our love's connection.
+                          <h2 className="text-xl font-bold tracking-tight text-gray-800 sm:text-2xl text-center">
+                            A limited-issue release of music Solana NFTs.
                           </h2>
 
                           <div className="-m-3 flex items-start rounded-lg p-3 transition duration-150 ease-in-out hover:bg-gray-50">
@@ -123,13 +97,12 @@ export default function Home() {
                               />
                             </div>
                             <div className="ml-4">
-                              <p className="text-base font-medium text-gray-900">
-                                Take his love.
-                              </p>
-                              <p className="mt-1 text-sm text-gray-500 mb-2">
-                                please...
-                              </p>
-                              <MintButton collectionId="22c6ecefe246" />
+                              <MintButton collectionId="c1167a3a9f1e"/>
+                              <MintButton collectionId="65fc36c9add8" />
+                              <MintButton collectionId="b197979c8f39" />
+                              <MintButton collectionId="48c9ca3dfd60" />
+                              <MintButton collectionId="f839d0c91ade" />
+                              <MintButton collectionId="162cd4ed2b30" />
                             </div>
                           </div>
                         </div>
